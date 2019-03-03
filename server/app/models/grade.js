0b978859c6245a535604d487d4977847963ca388
Anatole Abé <anatoleabe@gmail.com>
2019-03-03 13:10:08 +0100

    set json and models

--- server/app/models/grade.js
+++ server/app/models/grade.js
@@ -3,21 +3,19 @@ var mongoose        = require('mongoose');
 var Schema = mongoose.Schema;
 var ObjectId = mongoose.Schema.Types.ObjectId;
 
-// Structure schema
-var StructureSchema = new Schema({
+// Position schema
+var PositionSchema = new Schema({
     identifier: { type: String, required: true },
     name: { type: String, required: true },
     code: { type: String, required: true },
-    structureRootID: { type: ObjectId, required: true },
-    departement: { type: String, required: false },
-    rank: { type: String, required: false },
-    type: { type: String, required: false },
+    corpsID: { type: ObjectId, required: false },
+    categoryID: { type: ObjectId, required: false },
     lastModified: { type: Date, default: Date.now, required: true },
     created: { type: Date, default: Date.now, required: true }
 });
 
 //Define Models
-var Structure = mongoose.model('Structure', StructureSchema);
+var Position = mongoose.model('Position', PositionSchema);
 
 // Export Models
-exports.Structure = Structure;
+exports.Position = Position;
